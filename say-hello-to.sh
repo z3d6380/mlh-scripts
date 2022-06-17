4bd1fd593fc8d552a99524e200882ced84b9be0d
Luis Moraguez <lmoraguez6380@floridapoly.edu>
2022-06-17 19:19:32 +0000

    Week 3 - Exercise 1

---
+++ say-hello-to.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# File: say-hello-to.sh
+# Written by: Luis Moraguez
+# Description: Script that takes command line arguments and returns them in an echo statement saying "Hello, X Y Z"
+
+echo "Hello, $@"
