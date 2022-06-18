cd373947629a73e602af66f288b9a3b0c24573c6
Luis Moraguez <lmoraguez6380@floridapoly.edu>
2022-06-18 15:03:44 +0000

    Week 3 - Exercises 5.1 thru 5.4

--- 5.3.sh
+++ 5.3.sh
@@ -3,4 +3,6 @@
 # File: 5.3.sh
 # Written By: Luis Moraguez
 # Description: Print the number of requests by status-code for all status codes in the log file
+# Usage: ./5.3.sh {path_to_log}
 
+cat $1 | sort -nk9 | cut -d ' ' -f 9 | uniq -c | awk '{print $2": "$1}'
