234c279a0972972b867e0e341e6324facd611a8c
Luis Moraguez <lmoraguez6380@floridapoly.edu>
2022-06-18 14:49:08 +0000

    Week 3 - Exercises 5.1 thru 5.4

--- 5.2.sh
+++ 5.2.sh
@@ -3,5 +3,6 @@
 # File: 5.2.sh
 # Written By: Luis Moraguez
 # Description: Print the size of the 5 largest requests in bytes
+# Usage: ./5.2.sh {path_to_log}
 
-
+cat $1 | sort -r -nk10 | head -n 5
--- 5.3.sh
+++ 5.3.sh
@@ -1,6 +1,6 @@
-#/usr/bin/env python
+#/usr/bin/env bash
 
-# File: 5.3.py
+# File: 5.3.sh
 # Written By: Luis Moraguez
 # Description: Print the number of requests by status-code for all status codes in the log file
 
